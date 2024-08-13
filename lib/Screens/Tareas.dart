ba7a201bf6dcb7c4c858250494694d26448e065d
Elias Walton <elias_walton@hotmail.com>
2024-08-12 20:45:41 -0600

    He creado la pantalla de tareas

--- lib/Screens/Tareas.dart
+++ lib/Screens/Tareas.dart
@@ -1,10 +1,91 @@
 import 'package:flutter/material.dart';
 
-class ListaTareas extends StatelessWidget {
+class ListaTareas extends StatefulWidget {
   const ListaTareas({super.key});
 
   @override
+  State<ListaTareas> createState() => _ListaTareasState();
+}
+
+class _ListaTareasState extends State<ListaTareas> {
+  final List<String> tasks = [
+    'Tarea de Matemáticas',
+    'Foro de Historia',
+    'Examen de Física',
+    'Tarea de Química',
+    'Foro de Literatura'
+  ];
+
+  void _addTask(String task) {
+    setState(() {
+      tasks.add(task);
+    });
+  }
+
+  void _showAddTaskDialog() {
+    TextEditingController taskController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Agregar Nueva Tarea'),
+          content: TextField(
+            controller: taskController,
+            decoration: const InputDecoration(hintText: 'Escribe la tarea aquí'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Agregar'),
+              onPressed: () {
+                _addTask(taskController.text);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tareas Pendientes'),
+      ),
+      body: ListView.separated(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              tasks[index],
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              
+            ),
+            tileColor: const Color.fromARGB(255, 242, 242, 242),
+            
+            
+          );
+        },
+        separatorBuilder: (context, index) {
+          return const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddTaskDialog,
+        tooltip: 'Agregar Tarea',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
